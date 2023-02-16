2b36a5921aa7236fb81a6020d11c4c7bc8b0865b
Павел Тесленко <84148561+1wz@users.noreply.github.com>
2023-02-17 00:50:28 +0300

    Initial commit

---
+++ Assets/_Root/Scripts/Ui/MainMenu/BackToMenuView.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace Ui.MainMenu
+{
+    internal class BackToMenuView : MonoBehaviour
+    {
+        [SerializeField] private Button _backToMenuButton;
+
+        public void Init(UnityAction backToMenu) =>
+            _backToMenuButton.onClick.AddListener(backToMenu);
+
+        private void OnDestroy() =>
+            _backToMenuButton.onClick.RemoveAllListeners();
+    }
+}
